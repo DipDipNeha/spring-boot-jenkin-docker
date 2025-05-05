4467456ab0d71886788c97ec1bb9a376017c33b2
Dipak <kumardipak962@gmail.com>
2025-05-05 09:29:42 +0530

    chnage to check CICD pipeline

--- src/test/java/com/jenkins/docker/SpringBootJenkinsDockerApplicationTests.java
+++ src/test/java/com/jenkins/docker/SpringBootJenkinsDockerApplicationTests.java
@@ -3,11 +3,14 @@ package com.jenkins.docker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class SpringBootJenkinsDockerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
+		System.out.println("Hello Dipak Kumar ");
 	}
 
 }
